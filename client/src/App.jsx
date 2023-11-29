9bd23f44c5d4ed9766d56b175c831997f17a3e58
hhh <msmservices1846@gmail.com>
2023-11-29 18:02:11 +0530

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import AddEmployee from './components/AddEmployee';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <AddEmployee/>
+    </>
+  )
+}
+
+export default App
